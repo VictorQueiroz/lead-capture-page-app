5cc113ed971f4fe8a0504d27af53177a4f8bfcb6
Victor Queiroz <victorcqueirozg@gmail.com>
2023-05-12 16:05:26 -0300

    deal with `className` property on `Section` component
    Signed-off-by: Victor Queiroz <victorcqueirozg@gmail.com>

--- src/Section.tsx
+++ src/Section.tsx
@@ -1,14 +1,18 @@
 import './Section.scss';
+import { JSX } from 'react';
+import classNames from 'classnames';
 
 export default function Section({
   title,
+  className,
   children
 }: {
   title: string;
   children: JSX.Element;
+  className?: string;
 }) {
   return (
-    <div className="section">
+    <div className={classNames('section', className)}>
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
