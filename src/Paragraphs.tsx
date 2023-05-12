445b3e9a66ea4f5b8f2df14f119014c801876496
Victor Queiroz <victorcqueirozg@gmail.com>
2023-05-12 16:05:04 -0300

    fix Paragraphs component
    Signed-off-by: Victor Queiroz <victorcqueirozg@gmail.com>

--- src/Paragraphs.tsx
+++ src/Paragraphs.tsx
@@ -1,20 +1,28 @@
-import { Fragment, ReactNode } from 'react';
+import { Fragment, JSX } from 'react';
 
-export default function Paragraphs({ children }: { children: ReactNode[][] }) {
+export default function Paragraphs({
+  children
+}: {
+  children: (JSX.Element | string[])[];
+}) {
   return (
     <>
       <div className="row">
         <div className="col-lg-12">
-          {children.map((sentences, i) => (
-            <p key={i}>
-              {sentences.map((s, j, list) => (
-                <Fragment key={j}>
-                  {s}
-                  {j !== list.length - 1 ? ' ' : null}
-                </Fragment>
-              ))}
-            </p>
-          ))}
+          {children.map((sentences, i) =>
+            Array.isArray(sentences) ? (
+              <p key={i}>
+                {sentences.map((s, j, list) => (
+                  <Fragment key={j}>
+                    {s}
+                    {j !== list.length - 1 ? ' ' : null}
+                  </Fragment>
+                ))}
+              </p>
+            ) : (
+              sentences
+            )
+          )}
         </div>
       </div>
     </>
